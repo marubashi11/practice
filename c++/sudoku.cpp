2e4f46ec2ff5de226ec8bcd77a7810e983420c55
marubashi11 <marubashi.yuuki641@mail.kyutech.jp>
2017-09-17 18:39:43 +0900

    数独プログラム作成失敗。
    適当に問題を作成しようとすると、解のない問題ばかり作成してしまうことが判明。別の方法で問題を作成すべき。

--- c++/sudoku.cpp
+++ c++/sudoku.cpp
@@ -1,60 +1,102 @@
 #include <iostream>
 #include <random>
-#define SIZE 6
-
+#define SIZE 9 //盤面の大きさ。3の倍数推奨。
+#define SET 50 //始めにランダムでセットする数字の個数。
 /*
-
-   ----------------------------
-  0| 0  1  2| 3  4  5| 6  7  8|
-  1| 0  1  2| 3  4  5| 6  7  8|
-  2| 0  1  2| 3  4  5| 6  7  8|
-   ----------------------------
-  3| 0  1  2| 3  4  5| 6  7  8|
-  4| 0  1  2| 3  4  5| 6  7  8|
-  5| 0  1  2| 3  4  5| 6  7  8|
-   ----------------------------
-  6| 0  1  2| 3  4  5| 6  7  8|
-  7| 0  1  2| 3  4  5| 6  7  8|
-  8| 0  1  2| 3  4  5| 6  7  8|
-  ----------------------------
-
+   x 0 1 2   3 4 5   6 7 8
+ y -------------------------
+  0| ?     |       |       |
+  1|   0   |   1?  |   2   |
+  2|       |       |     ? |
+   -------------------------
+  3|       |       | ?     |
+  4|   3?  |   4   |   5   |
+  5|       |     ? |       |
+   -------------------------
+  6|       | ?     |       |
+  7|   6   |   7   |   8?  |
+  8|     ? |       |       |
+   -------------------------
+*'?'の位置にまずランダムに数字を配置し、それをcomが解くように数字を埋めていく。
+board[y][x]が属するブロックの左上の座標(x1,y1)は、
+    x1 = (x / 3) * 3, y1 = (y / 3) * 3
+で求まる。
+ブロック番号bは、
+    b = (y / 3) * 3 + x / 3
+で求まる。
 */
 
 class Board{
  public:
-  Board(); //numbers_すべてに0を代入。
+  Board(); //board_すべてに0を代入し、適当にいくつかの数字配置。
+  bool check_line(int, int);
+  bool check_column(int, int);
+  bool check_block(int, int, int);
+  bool check(int, int, int); //上記3つのチェックを行う。
   void set_numbers(); //条件を満たすように数字を散りばめる。
-  void out_numbers(); //盤面を出力。
+  bool check_q(); //問題として成立しているか確認。
+  void out_board(); //盤面を出力。
  private:
-  int numbers_[SIZE][SIZE];
+  int board_[SIZE][SIZE];
 };
 Board::Board(){
   for(int i = 0; i < SIZE; i++)
     for(int j = 0; j < SIZE; j++)
-      numbers_[i][j] = 0;
+      board_[i][j] = 0;
+}
+bool Board::check_line(int num, int y){
+  for(int i = 0; i < SIZE; i++)
+    if(board_[y][i] == num) return false;
+  return true;
+}
+bool Board::check_column(int num, int x){
+  for(int i = 0; i < SIZE; i++)
+    if(board_[i][x] == num) return false;
+  return true;
+}
+bool Board::check_block(int num, int x, int y){
+  int x1 = (x / 3) * 3;
+  int y1 = (y / 3) * 3;
+  for(int i = 0; i < 3; i++)
+    for(int j = 0; j < 3; j++)
+      if(board_[y1 + i][x1 + j] == num) return false;
+  return true;
+}
+bool Board::check(int num, int x, int y){
+  return check_line(num, y) && check_column(num, x) && check_block(num, x, y);
 }
 void Board::set_numbers(){
-  std::random_device rn;
-  std::mt19937 mrn(rn()); //軽量版乱数生成器。
-  int num;
-
-  for(int i = 0; i < SIZE; i++){
-    for(int j = 0; j < SIZE; j++){
-      while(true){ //numが審査を通るまでループ
-        bool check = true;
-        num = mrn() % SIZE + 1;
-        for(int k = 1; k <= i; k++)
-          if(numbers_[i - k][j] == num) check = false;
-        for(int l = 1; l <= j; l++)
-          if(numbers_[i][j - l] == num) check = false;
-        if(check) break;
-      }
-      numbers_[i][j] = num;
-      std::cout << i + 1 << "行" << j + 1 << "列セット完了" << std::endl;
-    }
+  std::random_device rnd;
+  int num, X, Y;
+  for(int s = 0; s < SET; s++){ //ランダムでSET個の数字セット。
+    X = rnd() % 9;
+    Y = rnd() % 9;
+    num = rnd() % 9 + 1;
+    if(board_[Y][X] == 0 && check(num, X, Y)) board_[Y][X] = num;
+    else s--;
   }
+  std::cout << "初期配置完了!" << std::endl;
+
+  for(int i = 0; i < 9; i++) //数独を解きながら数字セット。
+    for(int j = 0; j < 9; j++)
+      if(board_[i][j] == 0)
+        for(int n = 1; n <= 9; n++){
+          if(check(n, j, i)){
+            board_[i][j] = n;
+            break;
+          }
+        }
 }
-void Board::out_numbers(){
+bool Board::check_q(){
+  for(int i = 0; i < 9; i++)
+    for(int j = 0; j < 9; j++)
+      if(board_[i][j] == 0){
+        std::cout << "作成失敗" << std::endl;
+        return false;
+      }
+  return true;
+}
+void Board::out_board(){
   std::cout << std::endl;
   for(int i = 0; i < SIZE * 2 + (SIZE / 3) * 2 + 1; i++)
     std::cout<< "-";
@@ -62,7 +104,7 @@ void Board::out_numbers(){
   for(int i = 0; i < SIZE; i++){
     std::cout << "|";
     for(int j = 0; j < SIZE; j++){
-      std::cout << " " << numbers_[i][j];
+      std::cout << " " << board_[i][j];
       if(j % 3 == 2) std::cout << " |";
     }
     std::cout << std::endl;
@@ -76,8 +118,11 @@ void Board::out_numbers(){
 
 int main(){
   Board sudoku;
-  sudoku.set_numbers();
-  sudoku.out_numbers();
-
+  while(true){
+    sudoku.set_numbers();
+    sudoku.out_board();
+    if(sudoku.check_q()) break;
+  }
+  sudoku.out_board();
   return 0;
 }
